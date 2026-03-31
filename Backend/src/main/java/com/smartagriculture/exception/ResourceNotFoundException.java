afc6ea1fc5199f5807b77563490cefd2dcbb19da
snehathesingh-stack <snehathesingh@gmail.com>
2026-03-31 15:09:19 +0530

    Delete Backend directory

--- Backend/src/main/java/com/smartagriculture/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.smartagriculture.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- Backend/src/main/java/com/smartagriculture/repository/FarmerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.smartagriculture.repository;
-
-import com.smartagriculture.entity.Farmer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FarmerRepository extends JpaRepository<Farmer, Long> {
-}
